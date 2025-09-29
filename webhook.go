fed8e8d14c7b2d0f318ab02bc6b9b06b2c84766d
idanya <danhilkau2@gmail.com>
2025-09-29 11:00:16 +0300

    e

--- webhook.go
+++ webhook.go
@@ -20,25 +20,25 @@ type WebhookConvert struct {
 }
 
 type Webhook struct {
-	Type              string         `json:"type"`
-	UUID              string         `json:"uuid"`
-	OrderId           string         `json:"order_id"`
-	Amount            string         `json:"amount"`
-	PaymentAmount     string         `json:"payment_amount"`
-	PaymentAmountUSD  string         `json:"payment_amount_usd"`
-	MerchantAmount    string         `json:"merchant_amount"`
-	Commission        string         `json:"commission"`
-	IsFinal           bool           `json:"is_final"`
-	Status            string         `json:"status"`
-	From              string         `json:"from"`
-	WalletAddressUUID string         `json:"wallet_address_uuid"`
-	Network           string         `json:"network"`
-	Currency          string         `json:"currency"`
-	PayerCurrency     string         `json:"payer_currency"`
-	AdditionalData    string         `json:"additional_data"`
-	Convert           WebhookConvert `json:"convert"`
-	TxId              string         `json:"txid"`
-	Sign              string         `json:"sign"`
+	Type              string          `json:"type"`
+	UUID              string          `json:"uuid"`
+	OrderId           string          `json:"order_id"`
+	Amount            string          `json:"amount"`
+	PaymentAmount     string          `json:"payment_amount"`
+	PaymentAmountUSD  string          `json:"payment_amount_usd"`
+	MerchantAmount    string          `json:"merchant_amount"`
+	Commission        string          `json:"commission"`
+	IsFinal           bool            `json:"is_final"`
+	Status            string          `json:"status"`
+	From              string          `json:"from"`
+	WalletAddressUUID string          `json:"wallet_address_uuid"`
+	Network           string          `json:"network"`
+	Currency          string          `json:"currency"`
+	PayerCurrency     string          `json:"payer_currency"`
+	AdditionalData    string          `json:"additional_data"`
+	Convert           *WebhookConvert `json:"convert"`
+	TxId              string          `json:"txid"`
+	Sign              string          `json:"sign"`
 }
 
 type ResendWebhookRequest struct {
